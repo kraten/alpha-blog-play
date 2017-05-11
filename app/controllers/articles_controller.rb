0bfab1d6ceac3d1e94aa65bd16f66a94eb682e27
Rails Playground <railsplay@gmail.com>
2017-05-11 04:41:46 +0000

    Add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
   end
   
   def create
@@ -17,6 +25,17 @@ class ArticlesController < ApplicationController
     end
     
     # redirect_to articles_show(@article)
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      flash[:notice] = "Article successfully edited"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
   
   def show
